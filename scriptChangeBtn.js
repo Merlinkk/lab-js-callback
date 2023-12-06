25ab0ac0b5a4f55fa3bdcd89e014826c6b8b01cb
Merlinkk <anshulkashyap2504@gmail.com>
2023-12-06 11:04:18 +0530

    assignment done

---
+++ scriptChangeBtn.js
@@ -0,0 +1,32 @@
+// let currentScript = 'withoutCallback.js';
+// currentScript = document.getElementById('scriptC').getAttribute('src')
+// const button = document.getElementById('button');
+// const script = document.getElementById('scrpitC');
+// const callid = document.getElementById('callid');
+
+// button.textContent = `Current Script: ${currentScript}`;
+
+// button.addEventListener('click', function() {
+//     currentScript = currentScript === 'withoutCallback.js' ? 'callback.js' : 'withoutCallback.js';
+//     script.setAttribute('src', `./${currentScript}`);
+//     document.getElementById('content').innerHTML=""
+//     button.textContent = `Current Script: ${currentScript}`;
+//     setTimeout(()=>{
+//         executeScript()
+//       },1000)
+// });
+
+// executeScript()
+
+// function executeScript() {
+    
+//     if (currentScript === 'withoutCallback.js') {
+//         getCookies(cookies);
+//         createCookies1(newCookie);
+//         console.log('withoutCallback.js loaded');
+
+//     } else if (currentScript === 'callback.js') {
+//         createCookies(newCookie);
+//         console.log('callback.js loaded');
+//     }
+// }
